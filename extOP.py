4ecb50be71937db8406c9e38d3a72af902ccb484
NicolasAudepin <nicolas.audepin@gmail.com>
2019-02-01 17:18:48 +0100

    nom plus cour
    chiant a ecrire

--- extOP.py
+++ extOP.py
@@ -1,8 +1,11 @@
+print("\nEFOP I CHOOSE YOUUU")
+
 import sys
 import cv2
 import os
-from sys import plateform
+from sys import platform
 import argparse
+
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 print("dir_path = "+ dir_path)
