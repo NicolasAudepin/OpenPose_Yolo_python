5f84ed5f1ed8d5a4f3604c5574f9fa9c81b3b0d3
NicolasAudepin <nicolas.audepin@gmail.com>
2019-02-03 16:59:24 +0100

    premier error

--- extOP.py
+++ extOP.py
@@ -1,3 +1,6 @@
+#heavily inspired by https://github.com/CMU-Perceptual-Computing-Lab/openpose/blob/master/examples/tutorial_api_python/2_whole_body_from_image.py
+
+
 print("_________________________________")
 print("EXTRACTING SKELETON FROM OPENPOSE")
 
@@ -12,13 +15,33 @@ import argparse
 
 print("setting the paths")
 dir_path = os.path.dirname(os.path.realpath(__file__))
+print("dir_path = "+ dir_path)
 
 try:
-    print("dir_path = "+ dir_path)
+    
     sys.path.append('../../')
+    sys.path.append('../../')
+    print("importing openpose as op")
+    from openpose import pyopenpose as op
 except ImportError as e:
     print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
     raise e
+
+# 
+params = dict()
+params["model_folder"] = "../../../models/"
+params["face"] = True
+params["hand"] = True
+print(params)
+
+
+#Starting OP
+print('creating OPWraper')
+opWrapper = op.WrapperPython()
+opWrapper.configure(params)
+opWrapper.start()
+
+
 
 print("\nTHE END")
 print("_________________________________\n")
