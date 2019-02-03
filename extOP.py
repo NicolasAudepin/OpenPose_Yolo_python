4777b505384ab24ee8be60f89430f4297c0323cc
NicolasAudepin <nicolas.audepin@gmail.com>
2019-02-03 15:52:08 +0100

    Update extOP.py
    blabla

--- extOP.py
+++ extOP.py
@@ -1,5 +1,8 @@
-print("\nEFOP I CHOOSE YOUUU")
+print("_________________________________")
+print("EXTRACTING SKELETON FROM OPENPOSE")
 
+
+print("importing libs")
 import sys
 import cv2
 import os
@@ -7,6 +10,15 @@ from sys import platform
 import argparse
 
 
+print("setting the paths")
 dir_path = os.path.dirname(os.path.realpath(__file__))
-print("dir_path = "+ dir_path)
 
+try:
+    print("dir_path = "+ dir_path)
+    sys.path.append('../../')
+except ImportError as e:
+    print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
+    raise e
+
+print("\nTHE END")
+print("_________________________________\n")
