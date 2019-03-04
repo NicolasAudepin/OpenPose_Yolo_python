0cfd60b7ff5ed02f25d987f20ae71bc0091b1d23
NicolasAudepin <nicolas.audepin@gmail.com>
2019-03-04 23:44:28 +0100

    Update OneFolderOP.py

--- OneFolderOP.py
+++ OneFolderOP.py
@@ -51,57 +51,71 @@ from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import get_graph_path, model_wh
 e = TfPoseEstimator(get_graph_path('cmu'), target_size=(432, 368))
 
+
+#this is the folder in wich all the pictures folders are
 print("FOLDERS ")
 kitchenDir = os.fsencode("../DATA/Cornell/kitchen")
+
+#we try to process the pictures of each data folder
 for folderNamebytes in sorted(os.listdir(kitchenDir)):
-
     print(folderNamebytes.strip().decode('utf-8'))
-    
+
+    outputfileName=folderNamebytes.strip().decode('utf-8')+".txt"
+    outputDir = os.fsencode("./OPdata")
+
+    #we try not to process the same data folder twice 
     print("is it already processed?")
+    alreadyprocessed=False
+    for DataNamebytes in sorted(os.listdir(outputDir)):
+        #print(DataNamebytes.strip().decode('utf-8') )
+        if (DataNamebytes.strip().decode('utf-8')==outputfileName):
+            alreadyprocessed=True
+            print("we already have a file with this name: "+DataNamebytes.strip().decode('utf-8'))
+    if ( alreadyprocessed==False ):
+        print("Nope !")
 
-    dataDir = os.fsencode("./OPdata")
-    for DataNamebytes in sorted(os.listdir(dataDir)):
-        print(DataNamebytes.strip().decode('utf-8') )
 
+    #if the folder have not already been processed we process each of its image and save all of it in a txt file
+    if ( alreadyprocessed==False ):
+        print("INITIALIZING THE OUTPUTS")
+        data={'positions':{}} #this is what will be saved as the output
+        body_parts={0:"Head",1:"mShoulder",2:"rShoulder",3:"rElbow",4:"rWrist",5:"lShoulder",6:"lElbow",7:"lWrist",8:"rHip",9:"rKnee",10:"rAnkle",11:"lHip",12:"lKnee",13:"lAnkle"}
 
-    print("INITIALIZING THE OUTPUTS")
-    data={'positions':{}}
-    body_parts={0:"Head",1:"mShoulder",2:"rShoulder",3:"rElbow",4:"rWrist",5:"lShoulder",6:"lElbow",7:"lWrist",8:"rHip",9:"rKnee",10:"rAnkle",11:"lHip",12:"lKnee",13:"lAnkle"}
+        print('THE ACTUAL PROCESSING HAPPEN HERE')
+        dataDir = os.fsencode("../DATA/Cornell/kitchen/"+folderNamebytes.strip().decode('utf-8')+"/rgbjpg")
+        n = 0
+        for fileNamebytes in sorted(os.listdir(dataDir)):
+            n = n + 1
+            try: #process one image and add its skeleton to data
+                print(folderNamebytes.strip().decode('utf-8')+" : "+fileNamebytes.strip().decode('utf-8'))
+                fileName = fileNamebytes.strip().decode('utf-8')
+                
+                image = cv.imread("../DATA/Cornell/kitchen/data_01-12-07/rgbjpg/"+fileName)
+                humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=resize_out_ratio) #this is the processing
+                #image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+                #cv.imwrite("./OPvideos/res"+fileName,image)
 
-    print('THE ACTUAL PROCESSING HAPPEN HERE')
+                # saving the skeleton in the data if there is one
+                try:
+                    human=humans[0]
+                    body_position={}
+                    for bPart in body_parts.keys():
+                        if bPart in human.body_parts:
+                            x=human.body_parts[bPart].x
+                            y=human.body_parts[bPart].y
+                            pos=[x,y]
+                            body_position[body_parts[bPart]]=pos
+                    data['positions'][n]=body_position
+                except:
+                    print("No Skeleton on this pic?")
+            except:
+                print("issues with the pic ?")
 
-    dataDir = os.fsencode("../DATA/Cornell/kitchen/"+folderNamebytes.strip().decode('utf-8')+"/rgbjpg")
-    n = 0
-    for fileNamebytes in sorted(os.listdir(dataDir)):
-        n = n + 1
-        print(fileNamebytes)
-        fileName = fileNamebytes.strip().decode('utf-8')
-
-        image = cv.imread("../DATA/Cornell/kitchen/data_01-12-07/rgbjpg/"+fileName)
-        #ProssessOnePic(image)
-        humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=resize_out_ratio)
-        image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
-        
-        #cv.imwrite("./OPvideos/res"+fileName,image)
-
-        # saving the skeleton in the data if there is one
-        try:
-            human=humans[0]
-            body_position={}
-            for bPart in body_parts.keys():
-                if bPart in human.body_parts:
-                    x=human.body_parts[bPart].x
-                    y=human.body_parts[bPart].y
-                    pos=[x,y]
-                    body_position[body_parts[bPart]]=pos
-            data['positions'][n]=body_position
-        except:
-            print("No Skeleton on this pic?")
-
-    print("SAVING THE SKELETONS FILE")
-    with open("./OPdata/"+folderNamebytes.strip().decode('utf-8')+".txt", 'w') as outfile:
-        json.dump(data, outfile, sort_keys = True, indent = 4,ensure_ascii = False)
-
+        print("SAVING THE SKELETONS FILE")
+        with open("./OPdata/"+folderNamebytes.strip().decode('utf-8')+".txt", 'w') as outfile:
+            json.dump(data, outfile, sort_keys = True, indent = 4,ensure_ascii = False)
+    else:
+        print("next folder ")
 
 print("\nTHE END")
 print("__________________________________________________")
