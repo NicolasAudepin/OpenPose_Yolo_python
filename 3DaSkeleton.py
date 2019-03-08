802560f379ea4d2a2283272e6eb35c7bb887070f
NicolasAudepin <nicolas.audepin@gmail.com>
2019-03-08 17:14:05 +0100

    il faut coller les skeletons a leurs image

--- 3DaSkeleton.py
+++ 3DaSkeleton.py
@@ -3,79 +3,80 @@ import numpy as np
 import json
 import cv2 as cv
 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def tronc(x,len):
+    ret = 0
+    if (x<2):
+        ret = 2
+    if (x>len-2):
+        ret = len-2
+    else:
+        ret = x
+    return ret
+
 def ZfromMat(x,y,mat):
     xlen=len(mat)
     ylen=len(mat[0])
-    xmat = int(round(x*xlen))
-    ymat = int(round(y*ylen))
+    xmat = int(round(x*xlen))-50
+    ymat = int(round(y*ylen))-100
+
+    #print(xmat)
+    #print(ymat)
+
+    xmat=int(tronc(xmat,xlen))
+    ymat=int(tronc(ymat,ylen))
     z=mat[xmat][ymat]
+    
     return [z,xmat,ymat]
 
 
 
-#loading the skeleton
+#loading the skeleton from the file
 with open("./OPdata/"+"data_02-10-35.txt", 'r') as skeletonData:
     skelData = json.load(skeletonData)
 skeletons = skelData[u'positions']
-skelist = []
-for key, skeletonDict in skeletons.iteritems():
+
+running = True
+nb = "0"
+while(running):
+    keyboard = str(input("next one ?"))
+    if (len(keyboard)==0):
+        print("next")
+        nb = str(int(nb)+1)
+    if(keyboard=="x"):
+        running = False
+        
+    if(len(keyboard)<5):
+        nb = int(keyboard)
+    else:
+        print("try again")
+    #print(str(nb)+"NB")
+    nbtxt = keyboard
+    while (len(nbtxt)<4):
+        nbtxt="0"+nbtxt
+
+    skeletonDict= skeletons[str(nb)]
     
     skeleton=[]
     for keyleton, boneDict in skeletonDict.iteritems():
         bone=[keyleton,boneDict]
         skeleton.append(bone)
-    temp = [key,skeleton]
-    skelist.append(temp)
 
-### it seems that printing too much data crash at random why?
-#n=0
-#m=0
-#for skeleton in skelist:
-#    try:
-#        print (skeleton[0])
-#        for bone in skeleton[1]:
-#           print(bone[1])
-#                
-#    except:
-#        print("!!!ERROR!!!")
-#        print("_____________")
-#        n+=1
-#        try:
-#            print (skeleton[0])
-#            for bone in skeleton[1]:
-#                print(bone[1])
-#        except:
-#            print("FUCKED TWICE")
-#            m+=1
-#print("_____________")
-#print(n)
-#print(m)
-###
-
-
-### loading the skeleton
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
-for skeleton in skelist:
-    frameNb= skeleton[0]
-    print(frameNb)
     X=[]
     Y=[]
     Z=[]
-    for bone in skeleton[1]:
+    for bone in skeleton:
         X.append(bone[1][0])
         Y.append(bone[1][1])
 
-    #loading the depth matrice
-    nbtxt = frameNb
-    while (len(nbtxt)<4):
-        nbtxt="0"+nbtxt
+
 
     depthData = scipy.io.loadmat("./data_02-10-35/depth/"+ nbtxt +".mat")
     depthMat = depthData["depth"]
     
-    for bone in skeleton[1]:
+    for bone in skeleton:
         z,xmat,ymat=ZfromMat(bone[1][1],bone[1][0],depthMat)
         Z.append(z)
         depthMat[xmat][ymat]=9000
@@ -105,7 +106,7 @@ for skeleton in skelist:
     plt.xlim([0,1])
     plt.ylim([1,0])
     plt.plot(X,Y,"o")
-    plt.title(frameNb)
+    plt.title(nbtxt)
     
     plt.subplot(2,2,3)
 
